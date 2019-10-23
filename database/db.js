3b564e193703c3e9dca2ec07ea46ee1b3833b0db
shubham <shubhamkatariya05@gmail.com>
2019-10-23 13:27:38 -0700

    db update

--- database/db.js
+++ database/db.js
@@ -4,9 +4,12 @@ const sequelize = new Sequelize("user_login", "admin", "Mission123", {
   host: "database-1.c7gf8ktybcod.us-west-1.rds.amazonaws.com",
   port: 3306,
   dialect: "mysql",
+  dialectOptions: {
+    ssl:'Amazon RDS'
+  },
 
   pool: {
-    max: 1000,
+    max: 10,
     min: 0,
     acquire: 30000,
     idle: 10000
@@ -15,5 +18,6 @@ const sequelize = new Sequelize("user_login", "admin", "Mission123", {
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+console.log("db-->", db);
 
 module.exports = db;
