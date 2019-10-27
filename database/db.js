a7bfd086ca8beb8c9ca1c7ecfe77f453bf611eb7
shubham05101994 <54881803+shubham05101994@users.noreply.github.com>
2019-10-27 02:34:57 -0700

    Updated db

--- database/db.js
+++ database/db.js
@@ -1,11 +1,11 @@
 const Sequelize = require("sequelize");
 const db = {};
-const sequelize = new Sequelize("user_login", "admin", "Mission123", {
-  host: "database-1.c7gf8ktybcod.us-west-1.rds.amazonaws.com",
-  port: 3306,
-  dialect: "mysql",
+const sequelize = new Sequelize("", "", "", {
+  host: "",
+  port: ,
+  dialect: "",
   dialectOptions: {
-    ssl:'Amazon RDS'
+    ssl:''
   },
 
   pool: {
